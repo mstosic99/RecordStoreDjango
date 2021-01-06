d61c9a19b60b9c3acb8e1ac176de711cf754311d
Mihajlo <mihajlostosic99@gmail.com>
2021-01-07 00:49:11 +0100

    frontend gotov

--- RecordStoreProject/recordstore/urls.py
+++ RecordStoreProject/recordstore/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'recordstore'
 urlpatterns = [
     path('', views.index, name='recordstore.index'),
-    path('hello/', views.hello, name='recordstore.hello')
+    path('records/', views.records, name='records'),
+    path('record/<int:id>', views.record, name='record'),
+    path('record/edit/<int:id>', views.edit, name='record'),
+    path('record/new', views.new, name='record'),
 ]
--- RecordStoreProject/recordstore/views.py
+++ RecordStoreProject/recordstore/views.py
@@ -1,14 +1,29 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Record
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Welcome')
+    if not request.user.is_authenticated:
+        return render(request, 'index.html', {'page_title': 'Records'})
+    else:
+        return redirect('recordstore:records')
 
 
-def hello(request):
-    return render(request, 'recordstore/hello.html')
+def record(request, id):
+    return render(request, 'record.html')
 
 
+def records(request):
+    tmp = Record.objects.all()
+    return render(request, 'records.html', {'records':tmp})
+
+
+def new(request):
+    return render(request, 'new.html')
+
+
+def edit(request, id):
+    return render(request, 'edit.html')
